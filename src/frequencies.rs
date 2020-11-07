79ba072138cf6b422045d129b1f4b3a8c4ee88c1
Michael Thaler <michael.thaler@dlr.de>
2020-11-07 13:42:27 +0100

    Added main.rs.

--- src/frequencies.rs
+++ src/frequencies.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn frequencies(s: String) -> HashMap<char, f64> {
+pub fn frequencies(s: String) -> HashMap<char, f64> {
     let mut counts: HashMap<char, i32> = HashMap::new();
     let s1 = s.to_lowercase();
     for c in s1.chars() {
@@ -13,4 +13,18 @@ fn frequencies(s: String) -> HashMap<char, f64> {
         frequencies.insert(c, count as f64 / length);
 	}
 	frequencies
+}
+
+pub fn printFrequencies(frequencies: HashMap<char, f64>, n: i32) {
+    let mut keys: Vec<&char> = frequencies.keys().collect();
+    keys.sort();
+    let mut count = 0;
+    for c in keys {
+        count += 1;
+        let f = frequencies.get(c).unwrap();
+        print!("{} -> {:.4}  ", c, f);
+        if count % n == 0 {
+            println!();
+        }
+    }
 }
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod frequencies;
+
+const MSG: &str =  "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, \n
+it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, \n
+we were all going direct to Heaven, we were all going direct the other way--in short, the period was so far like the present period, that some of its noisiest authorities insisted \n
+on its being received, for good or for evil, in the superlative degree of comparison only.";
+
+fn main() {
+    let f = frequencies::frequencies(String::from(MSG));
+    frequencies::printFrequencies(f, 4);
+}
